b1e334b527b7f16c65b2d7e41eb58ff309ad3cde
shimozuki <robbishimozuki@gmail.com>
2024-06-26 20:01:27 +0800

    master

--- resources/views/layouts/partials/menu/siswa.blade.php
+++ resources/views/layouts/partials/menu/siswa.blade.php
@@ -9,16 +9,6 @@
 </li>
 
 <li class="nav-item">
-    <a href="{{ route('siswa/kasus-siswa') }}"
-        class="nav-link {{ Route::currentRouteName() == 'siswa/kasus-siswa' ? 'active' : '' }}">
-        <i class="nav-icon fas fa-pen"></i>
-        <p>
-            Catatan Kasus
-        </p>
-    </a>
-</li>
-
-<li class="nav-item">
     <a href="{{ route('siswa/jadwal-bimbingan') }}"
         class="nav-link {{ Route::currentRouteName() == 'siswa/jadwal-bimbingan' ? 'active' : '' }}">
         <i class="nav-icon fas fa-calendar"></i>
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-light-primary elevation-3">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="{{ url('') }}/dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
+        <img src="{{ asset('logo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
