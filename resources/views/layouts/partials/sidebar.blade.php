a8844687f056d1b66d12f5b6f7d1824a26206311
shimozuki <robbishimozuki@gmail.com>
2024-06-26 20:03:11 +0800

    master

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -4,7 +4,7 @@
     <a href="index3.html" class="brand-link">
         <img src="{{ asset('logo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Pojok Curhat</span>
     </a>
 
     <!-- Sidebar -->
