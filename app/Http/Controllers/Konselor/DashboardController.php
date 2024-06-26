8acd4253fa1edbd552bbc66703cb23a5e9d4176b
shimozuki <robbishimozuki@gmail.com>
2024-06-26 23:40:50 +0800

    master

--- app/Http/Controllers/Konselor/DashboardController.php
+++ app/Http/Controllers/Konselor/DashboardController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\Konselor;
 
 use App\Http\Controllers\Controller;
+use App\Models\Konselor;
 use App\Models\Siswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $id = Auth::id();
+
+        $id_konselor = Konselor::select('nama')->where('id_user', $id)->first();
         $siswa = Siswa::count();
-        $data = ['title' => 'Halaman Dashboard', 'siswa' => $siswa];
+        $data = ['title' => 'Halaman Dashboard', 'siswa' => $siswa, 'nama' => $id_konselor];
 
         return view('konselor.dashboard.index', $data);
     }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1 class="m-0">Welcome {{ Auth::user()->name }}</h1>
+                <h1 class="m-0">Welcome Admin</h1>
             </div>
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
