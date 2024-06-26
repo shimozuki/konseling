0c3de269addea1b43ce260d4dbaae8c8e202ebe1
shimozuki <robbishimozuki@gmail.com>
2024-06-26 20:39:28 +0800

    js alert

--- resources/views/siswa/dashboard/index.blade.php
+++ resources/views/siswa/dashboard/index.blade.php
@@ -39,16 +39,21 @@
     <script>
     document.addEventListener('DOMContentLoaded', function() {
         if (Notification.permission !== "granted") {
-            alert('Please allow notifications for a better experience.');
-            Notification.requestPermission().then(function(result) {
-                if (result === 'granted') {
-                    new Notification('Terima kasih telah mengizinkan pemberitahuan!');
-                } else if (result === 'denied') {
-                    alert('Anda telah menolak pemberitahuan.');
-                }
-            });
+            let userResponse = confirm('Please allow notifications for a better experience. Click "OK" to allow notifications.');
+            if (userResponse) {
+                Notification.requestPermission().then(function(result) {
+                    if (result === 'granted') {
+                        new Notification('Terima kasih telah mengizinkan pemberitahuan!');
+                    } else if (result === 'denied') {
+                        alert('Anda telah menolak pemberitahuan.');
+                    }
+                });
+            } else {
+                alert('Anda telah menolak pemberitahuan.');
+            }
         }
     });
 </script>
+
     <!-- /.content -->
 @endsection
