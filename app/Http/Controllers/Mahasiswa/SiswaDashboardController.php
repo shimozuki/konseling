1a0a05aebc5ee2d67111dd7b40acbe9ba0643e49
shimozuki <robbishimozuki@gmail.com>
2024-06-26 23:36:30 +0800

    master

--- app/Http/Controllers/Mahasiswa/SiswaDashboardController.php
+++ app/Http/Controllers/Mahasiswa/SiswaDashboardController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\Mahasiswa;
 use App\Http\Controllers\Controller;
 use App\Models\Siswa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SiswaDashboardController extends Controller
 {
     public function index()
     {
+        $id = Auth::id();
+        $id_user = Siswa::select('nama')->where('id_user', $id)->first();
         $siswa = Siswa::count();
-        $data = ['title' => 'Halaman Dashboard', 'siswa' => $siswa];
+        $data = ['title' => 'Halaman Dashboard', 'siswa' => $siswa, 'nama' => $id_user->nama];
 
         return view('siswa.dashboard.index', $data);
     }
